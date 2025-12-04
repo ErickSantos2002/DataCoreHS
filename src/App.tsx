33751f2cb56125efaf7743a15c74c578402238f2
Erick Santos <ericksantosdantas@gmail.com>
2025-12-04 15:59:54 -0300

    botão central

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import "./styles/index.css"; // Importa o Tailwind e estilos globais
 import AppRoutes from "./router";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
+import CentralButton from "./components/CentralButton";
 
 // Rotas onde o layout (Header/Sidebar) não deve aparecer (ex: login)
 const noLayoutRoutes = ["/login"];
@@ -18,15 +19,20 @@ const App: React.FC = () => {
   }
 
   return (
-    <div className="h-screen flex flex-col bg-gray-100 dark:bg-darkBlue text-gray-900 dark:text-gray-100 transition-colors">
-      <Header />
-      <div className="flex flex-col lg:flex-row flex-1 overflow-hidden">
-        <Sidebar />
-        <main className="flex-1 overflow-auto bg-gray-100 dark:bg-darkBlue transition-colors">
-          <AppRoutes />
-        </main>
+    <>
+      <div className="h-screen flex flex-col bg-gray-100 dark:bg-darkBlue text-gray-900 dark:text-gray-100 transition-colors">
+        <Header />
+        <div className="flex flex-col lg:flex-row flex-1 overflow-hidden">
+          <Sidebar />
+          <main className="flex-1 overflow-auto bg-gray-100 dark:bg-darkBlue transition-colors">
+            <AppRoutes />
+          </main>
+        </div>
       </div>
-    </div>
+
+      {/* Botão Flutuante Central HS */}
+      <CentralButton />
+    </>
   );
 };
 
